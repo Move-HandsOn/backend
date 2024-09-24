753bd96e7644f3a43b6ba0535330196da44e5861
Amanda Jauregui <amandajauregui.dev@gmail.com>
2024-09-24 15:47:46 -0300

    feat: created users module with functions to find user by email, ID, and update user

---
+++ src/users/users.service.ts
@@ -0,0 +1,119 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {}
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+
+  async updateUser(email: string, updateUserDto: UpdateUserDto) {
+    const user = await this.findUserByEmail(email);
+
+    if (!user) {
+      throw new HttpException('User not found.', HttpStatus.NOT_FOUND);
+    }
+
+    const hashedPass = this.generateHash(updateUserDto.password);
+
+    try {
+      return await this.prismaService.user.update({
+        data: {
+          ...updateUserDto,
+          password: hashedPass,
+        },
+        where: {
+          id: user.id,
+        },
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new HttpException(
+          'Database error or invalid query',
+          HttpStatus.BAD_REQUEST
+        );
+      }
+
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  async findUserByEmail(email: string) {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new HttpException(
+          'Database error or invalid query',
+          HttpStatus.BAD_REQUEST
+        );
+      }
+
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  async findUserById(id: string) {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new HttpException(
+          'Database error or invalid query',
+          HttpStatus.BAD_REQUEST
+        );
+      }
+
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  generateHash(password: string): string {
+    return bcrypt.hashSync(password, 10);
+  }
+}
