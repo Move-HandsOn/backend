899b7db5f410726759db2f60f6188404e463ede1
Amanda Jauregui <amandajauregui.dev@gmail.com>
2024-10-07 15:27:16 -0300

    chore: setup nodemailer with .env example for email transport configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,25 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.EMAIL_HOST,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      },
+    }),
+  ],
   providers: [
     {
       provide: APP_GUARD,
