dc009b900462a228084d964ea77dc6fa625f937d
Amanda Jauregui <amandajauregui.dev@gmail.com>
2024-10-08 14:15:40 -0300

    refactor: move MailerModule to its own dedicated module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { APP_GUARD } from '@nestjs/core';
-import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule } from '@nestjs/config';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -14,15 +14,7 @@ import { ConfigModule } from '@nestjs/config';
     UsersModule,
     AuthModule,
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.EMAIL_HOST,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD,
-        },
-      },
-    }),
+    MailModule,
   ],
   providers: [
     {
