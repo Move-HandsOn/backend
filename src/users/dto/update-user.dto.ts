a27637130ee14091f13682936915ef7f9f5ff4da
Amanda Jauregui <amandajauregui.dev@gmail.com>
2024-10-16 18:14:44 -0300

    refactor: remove field check logic from updateUserDto, move to user service; add image upload function to update and create user; refactor update function

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,14 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNotEmpty, ValidateIf } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  // validation to ensure at least one field is present in the update
-  @ValidateIf((object) => {
-    return !Object.values(object).some(
-      (value) => value !== null && value !== undefined
-    );
-  })
-  @IsNotEmpty({ message: 'At least one field must be updated' })
-  updateError?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,8 @@ import {
   Query,
   HttpCode,
   HttpStatus,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -20,6 +22,8 @@ import { User } from '@prisma/client';
 import { Public } from 'src/decorators/public.decorator';
 import { UserPresenter } from './presenter/user.presenter';
 import { ChangePasswordDto } from './dto/changePasswordDto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileUploadDTO } from 'src/supabase/dto/upload.dto';
 
 @Controller()
 export class UsersController {
@@ -27,9 +31,13 @@ export class UsersController {
 
   @Public()
   @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(FileInterceptor('profile_image'))
   @Post('user')
-  async create(@Body() createUserDto: CreateUserDto) {
-    const user = await this.usersService.create(createUserDto);
+  async create(
+    @Body() createUserDto: CreateUserDto,
+    @UploadedFile() file: FileUploadDTO
+  ) {
+    const user = await this.usersService.create(createUserDto, file);
 
     if (!user) {
       throw new ConflictException('Email already exists.');
@@ -50,11 +58,17 @@ export class UsersController {
     return userFound;
   }
 
+  @UseInterceptors(FileInterceptor('profile_image'))
   @Patch('profile')
-  async update(@GetUser() user: User, @Body() updateUserDto: UpdateUserDto) {
+  async update(
+    @GetUser() user: User,
+    @Body() updateUserDto: UpdateUserDto,
+    @UploadedFile() file: FileUploadDTO
+  ) {
     const updatedUser = await this.usersService.update(
       user.email,
-      updateUserDto
+      updateUserDto,
+      file
     );
 
     if (!updatedUser) {
